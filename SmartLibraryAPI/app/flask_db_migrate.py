5a8e5b9a188c6a4f324398e4098d17ba07b3507b
awse12311 <awse12311@gmail.com>
2024-02-05 13:28:00 +0800

    更新models程式碼套件引用方式

--- SmartLibraryAPI/app/flask_db_migrate.py
+++ SmartLibraryAPI/app/flask_db_migrate.py
@@ -9,13 +9,13 @@
 
 from flask import Flask
 from flask_migrate import Migrate
-from app.extensions import first_init_db
+from .extensions import db
 from app import create_app
 
 app = create_app()
 
 # 初始化 Flask-Migrate
-migrate = Migrate(app, first_init_db)
+migrate = Migrate(app, db)
 
 if __name__ == '__main__':
     app.run()
