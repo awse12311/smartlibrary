4e195ca7b4dd4a73b61a8794bc5c7aae00db7ca7
Huanggg <ned41602@gmail.com>
2024-02-21 11:51:29 +0800

    1

--- smartlibrary_web/app/services/recommend_service.py
+++ smartlibrary_web/app/services/recommend_service.py
@@ -6,4 +6,12 @@ class RecommendService:
         self.book_data = FetchService().get_all_books()
 
     def recommend_books_for_user(self, user_ins): #用list列出使用者的所有興趣項目
-        pass
+        recommend_books = [[book['title'], book['label'], book['average_rating']] for book in self.book_data if book in user_ins]
+
+        count = 10 / user_ins
+        recommend_list = []
+        for label in user_ins:
+            while len(recommend_list) < 10
+
+        pass 
+        
